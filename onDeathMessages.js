ac13a58a39358feaab3a6069c6b94f685fe62e32
Isaac Digangi <112435599+Isaac-digangi@users.noreply.github.com>
2025-11-05 10:33:16 -0600

    rewrite onDeath Messages

--- onDeathMessages.js
+++ onDeathMessages.js
@@ -4,15 +4,39 @@ function onPlayerKilledOtherPlayer(attackingPlayer, killedPlayer, damageDealt, w
 
 if (withItem === "FallDamage") {
   api.broadcastMessage(killedName + " fell from a high place", {color: "Red"});
-}else if (withItem === "Lava") {
-    api.broadcastMessage(killedName + " tried to swim in lava", {color: "Red"});
-}else if (withItem === "Stomp Damage Enchantment"){
+} else if (withItem === "Lava") {
+  api.broadcastMessage(killedName + " tried to swim in lava", {color: "Red"});
+} else if (withItem === "Stomp Damage Enchantment"){
 	api.broadcastMessage(killedName + " was crushed by " + attackerName + " with " + "Spiked Boots", {color:"Red"});
-}else if (withItem === "Firecracker" || "Firecracker Pebble"){
+}else if (withItem === "Firecracker"){
 	api.broadcastMessage(killedName + " went off with a bang", {color: "Red"});
-}else if (withItem === "Instant Damage Potion" || "Instant Damage Potion II" || "Splash Instant Damage Potion" || "Splash Instant Damage Potion II"){
+}else if (withItem === "Firecracker Pebble"){
+	api.broadcastMessage(killedName + " went off with a bang", {color: "Red"});
+}else if (withItem === "Instant Damage Potion"){
 	api.broadcastMessage(killedName + " was killed by magic", {color: "Red"});
+}else if (withItem === "Moonstone Explosive"){
+	api.broadcastMessage(killedName + " blew up", {color: "Red"});
+}else if (withItem === "Moonstone Remote Explosive"){
+	api.broadcastMessage(killedName + " blew up", {color: "Red"});
+}else if (withItem === "Fireball Block"){
+	if(killedName === attackerName){
+		api.broadcastMessage(killedName + " went up in flames", {color: "Red"});
+	}else{
+		api.broadcastMessage(killedName + " was fireballed by " + attackerName, {												color:"Red"});
+}
+}else if (withItem === "Iceball Block"){
+	if(killedName === attackerName){
+		api.broadcastMessage(killedName + " froze to death", {color: "Red"});
+	}else{
+		api.broadcastMessage(killedName + " was frozen by " + attackerName, {												color:"Red"});
+}
+}else if (withItem === "Spikes"){
+	if(killedName === attackerName){
+		api.broadcastMessage(killedName + " was impaled on spikes", {color: "Red"});
+	}else{
+	api.broadcastMessage(killedName + " was impaled on spikes by " + attackerName, {color: "Red"});
+}
 }else {
-    api.broadcastMessage(killedName + " was slain by " + attackerName + " with " + withItem, {color: "Red"});
+  api.broadcastMessage(killedName + " was slain by " + attackerName + " with " + withItem, {color: "Red"});
 }
 }
