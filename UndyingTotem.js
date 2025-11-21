dc3e8724e37fb80b3c81c4e55f407b8733d5c672
Isaac Digangi <112435599+Isaac-digangi@users.noreply.github.com>
2025-11-21 09:12:46 -0600

    added totem of undying world code
    Added functionality for player respawn and effects upon being killed by another player or mob.

--- UndyingTotem.js
+++ UndyingTotem.js
@@ -64,4 +64,65 @@ if (goldBlockCount >= goldBlockToRemove && moonstoneCount >= moonstoneToRemove) 
   api.sendMessage(myId, "You need 1 Block of Gold and 2 Moonstones to craft a Totem of Undying.");
 }
 
-//WORLD CODE ______ (unfinished)
+//WORLD CODE ______ (unfinished) (inventory drops on forced respawn)
+
+function onPlayerKilledOtherPlayer(mobId, playerId, damage, item)
+{
+	if(api.hasItem(playerId, "Gold Coin"))
+	{
+		api.forceRespawn(playerId, api.getPosition(playerId));
+		api.setShieldAmount(playerId, 40);
+		api.setHealth(playerId, 50);
+		api.applyEffect(playerId, "Health Regen", 5000, {inbuiltLevel: 2});
+		api.applyEffect(playerId, "Damage Reduction", 10000, {inbuiltLevel: 2});
+		api.removeItemName(playerId, "Gold Coin", 1)
+		playParticles(playerId);
+	}
+}
+function onMobKilledPlayer(mobId, playerId)
+{
+	if(api.hasItem(playerId, "Gold Coin"))
+	{
+		api.forceRespawn(playerId, api.getPosition(playerId));
+		api.setShieldAmount(playerId, 40);
+		api.setHealth(playerId, 50);
+		api.applyEffect(playerId, "Health Regen", 5000, {inbuiltLevel: 2});
+		api.applyEffect(playerId, "Damage Reduction", 10000, {inbuiltLevel: 2});
+		api.removeItemName(playerId, "Gold Coin", 1)
+		playParticles(playerId);
+	}
+}
+function playParticles(playerId)
+{
+		let [x, y, z] = api.getPosition(playerId);
+api.playParticleEffect({
+ 		dir1: [-1, -1, -1],
+ 		dir2: [1, 1, 1],
+  		pos1: [x + 2, y + 1.5, z + 2],
+    	pos2: [x - 2, y - 1.5, z - 2],
+    	texture: "square_particle",
+    	minLifeTime: 0.5,
+    	maxLifeTime: 2,
+    	minEmitPower: 4,
+    	maxEmitPower: 6,
+    	minSize: 0.1,
+   		maxSize: 0.5,
+    	manualEmitCount: 200,
+    	gravity: [0, -10, 0],
+    	colorGradients: [
+   	    {
+   	        timeFraction: 0,
+            minColor: [211, 214, 0, 0.5],
+            maxColor: [600, 500, 500, 0.8],
+        },
+    	],
+    	velocityGradients: [
+        {
+            timeFraction: 1,
+            factor: 0.2,
+            factor2: 1,
+        },
+    	],
+    	blendMode: 1,
+	})
+}
