94834ddcf75f0154ec35160963a44e2c6a410457
Isaac Digangi <112435599+Isaac-digangi@users.noreply.github.com>
2025-12-20 15:45:14 -0600

    Refactor chat message broadcasting for players

--- randomcodes.js
+++ randomcodes.js
@@ -1,5 +1,29 @@
-//dev / super / youtuber rank in chat messages
-api.broadcastMessage([{str:"[", style:{color:"#CEF3FF"}}, {icon:"wrench", style:{color:"#A4A4A4"}}, {str:" Dev] ", style:{color:"#CEF3FF"}}, {str:"[", style:{color:"#CEF3FF"}}, {icon:"youtube", style:{color:"#FE0000"}}, {str:" You", style:{color:"white"}}, {str:"Tuber", style:{color:"#FE0000"}}, {str:"] ", style:{color:"#CEF3FF"}}, {str:"[", style:{color:"#FFCC00"}}, {icon:"zap", style:{color:"#FFCC00"}}, {str:" Super] ", style:{color:"#FFCC00"}}, {str:api.getEntityName(myId), style:{color:"#DFF8FF"}}, {str:": lol", style:{color:"#DFF8FF"}}])
+// dev / super / youtuber rank in chat messages
+onPlayerChat = (playerId, chatMessage, channelName) => {
+
+    const playerName = api.getEntityName(playerId); 
+	if(playerName === "M1DNIGHT_SV"){
+    api.broadcastMessage([
+		/*
+        {str:"[", style:{color:"#CEF3FF"}},
+        {icon:"wrench", style:{color:"#A4A4A4"}},
+        {str:" Dev] ", style:{color:"#CEF3FF"}},
+		*/
+        {str:"[", style:{color:"#CEF3FF"}},
+        {icon:"youtube", style:{color:"#FE0000"}},
+        {str:" You", style:{color:"white"}},
+        {str:"Tuber", style:{color:"#FE0000"}},
+        {str:"] ", style:{color:"#CEF3FF"}},
+        {str:"[", style:{color:"#FFCC00"}},
+        {icon:"zap", style:{color:"#FFCC00"}},
+        {str:" Super] ", style:{color:"#FFCC00"}},
+        {str: playerName, style:{color:"blue"}},
+        {str:": " + chatMessage, style:{color:"white"}}
+	]);
+}
+
+    return false;
+};
 
 //gliding pose
 api. setPlayerPose (myId, "gliding")
