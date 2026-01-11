26d8b259abc16583f7ef608e51ff54d559ac826c
Isaac DiGangi <112435599+Isaac-digangi@users.noreply.github.com>
2026-01-11 17:59:17 -0600

    Implement onPlayerKilledOtherPlayer function
    Added a function to handle player kill messages with specific conditions.

--- onDeathMessages.js
+++ onDeathMessages.js
@@ -126,3 +126,26 @@ function tick() {
 	}
 }
 */
+/*
+
+function onPlayerKilledOtherPlayer(attackingPlayer, killedPlayer, damageDealt, withItem) {
+  const attackerName = api.getEntityName(attackingPlayer);
+  const killedName = api.getEntityName(killedPlayer);
+
+if (withItem === "FallDamage") {
+  api.broadcastMessage(killedName + " fell from a high place", {color: "Red"});
+} else if (withItem === "Lava") {
+  api.broadcastMessage(killedName + " tried to swim in lava", {color: "Red"});
+} else if (withItem === "Stomp Damage Enchantment"){
+	api.broadcastMessage(killedName + " was crushed by " + attackerName + " with " + "Spiked Boots", {color:"Red"});
+}else if (withItem === "Firecracker" || "Firecracker Pebble"){
+	api.broadcastMessage(killedName + " went off with a bang", {color: "Red"});
+}else if (withItem === "Instant Damage Potion" || "Instant Damage Potion II" || "Splash Instant Damage Potion" || "Splash Instant Damage Potion II"){
+	api.broadcastMessage(killedName + " was killed by magic", {color: "Red"});
+}else {
+  api.broadcastMessage(killedName + " was slain by " + attackerName + " with " + withItem, {color: "Red"});
+}
+}
+
+
+*/
